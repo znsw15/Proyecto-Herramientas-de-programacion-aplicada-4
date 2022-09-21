7c0a3ef220b288360e5959e8a3ed54054041d059
znsw15 <zulixa.salas@utp.ac.pa>
2022-09-21 11:29:36 -0500

    Pantalla de inicio Pantalla de Eleccion Pantalla de Votos

---
+++ app/src/main/java/com/example/proyecto1zsjn/Eleccion.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1zsjn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Eleccion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eleccion);
+    }
+}
