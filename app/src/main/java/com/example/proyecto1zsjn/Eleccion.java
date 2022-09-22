53f97ef913770e037e9715cf271546017f4e0f46
Juliok04 <julio.navas@utp.ac.pa>
2022-09-22 13:15:54 -0500

    Arreglo de todos los intent y la clase usuario y votos

--- app/src/main/java/com/example/proyecto1zsjn/Eleccion.java
+++ app/src/main/java/com/example/proyecto1zsjn/Eleccion.java
@@ -11,48 +11,49 @@ import android.widget.Toast;
 import android.widget.RadioButton;
 
 public class Eleccion extends AppCompatActivity {
-    RadioGroup rg;
-    Button btn1;
+    RadioGroup  radioGroup;
+    Button botonVoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eleccion);
-        Intent intent = getIntent();
-        rg = findViewById(R.id.rg);
-        btn1 = findViewById(R.id.btn1);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        radioGroup = findViewById(R.id.rg);
+        botonVoto = findViewById(R.id.btn1);
+
+        botonVoto.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                 float i = 0;
-                 int cand1 = 0;
-                 int cand2 = 0;
-                 int cand3 = 0;
-            @Override
-            public void onCheckedChanged(RadioGroup rg, int checkedId) {
-                switch (checkedId) {
-                    case R.id.rb1:
-                        cand1++;
-                        break;
-                    case R.id.rb2:
-                        cand2++;
-                        break;
-                    case R.id.rb3:
-                        cand3++;
-                        break;
+            public void onClick(View view){
+                int checkedId = radioGroup.getCheckedRadioButtonId();
+                Intent intent =new Intent(Eleccion.this, Votos.class);
+                if (checkedId == -1) {
+                    Toast.makeText(Eleccion.this, "No hay seleccion.", Toast.LENGTH_SHORT).show();
+                }else{
+                    findRadioButton(checkedId);
+                    startActivity(intent);
                 }
-                Toast.makeText(Eleccion.this, "Gracias por votar", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), Votos.class);
-                intent.putExtra("cand1", cand1);
-                intent.putExtra("cand2", cand2);
-                intent.putExtra("cand2", cand3);
-                startActivity(intent);
+
             }
-                });
-            }});
-    }}
+        });
+
+
+    }
+    private void findRadioButton(int checkedId) {
+        switch (checkedId) {
+            case R.id.rb1:
+                Usuario.cand1++;
+                break;
+            case R.id.rb2:
+                Usuario.cand2++;
+                break;
+            case R.id.rb3:
+                Usuario.cand3++;
+                break;
+
+        }
+    }
+}
 
 
 
--- app/src/main/java/com/example/proyecto1zsjn/Usuario.java
+++ app/src/main/java/com/example/proyecto1zsjn/Usuario.java
@@ -2,29 +2,10 @@ package com.example.proyecto1zsjn;
 
 import java.io.Serializable;
 
-public class Usuario implements Serializable {
+public class Usuario{
 
-    private String cedula;
+    public static int cand1=0, cand2=0, cand3=0;
     private String nombre;
 
-    public Usuario(String cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-    }
 
-    public String getCedula() {
-        return cedula;
-    }
-
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
