9592cd782772bda5295b04b021d2af0bd9b5b713
Juliok04 <julio.navas@utp.ac.pa>
2022-09-26 16:06:31 -0500

    Inicio Arreglado desde mi proyecto

--- app/src/main/java/com/example/proyecto1zsjn/MainActivity.java
+++ app/src/main/java/com/example/proyecto1zsjn/MainActivity.java
@@ -14,10 +14,14 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** Estas son las variables que usamos, etcedula es donde el usuario ingresa su cedula, el bt1 es para ingresar a votar y
+     * el bt2 es para ver cuanto porcentaje de votos tiene cada candidato, se crearon 2 Vectores que guardan los nombres y apellidos de los estudiantes
+     * estan en orden para identificar de quien es cada cedula*/
+
     EditText etcedula;
     Button btn1, btn2;
-    String[] cedulas = {"08-0944-000327", "03-0740-001394", "20-0053-004282", "08-0943-001867", "08-0937-000503", "08-0952-002444", "08-0943-000012", "08-0986-000549", "08-0957-001827",
-            "08-0940-001311", "08-0863-001620", "08-0964-00691", "08-0954-001126", "08-1039-001537", "08-0925-001168", "08-0940-001505", "08-0938-000925", "03-000743-002493","E-8-169755",
+    String[] cedulas = {"8-944-327", "3-740-1394", "20-53-4282", "8-943-1867", "8-937-503", "8-952-2444", "8-943-12", "8-986-549", "8-957-1827",
+            "8-940-1311", "8-863-1620", "8-964-691", "8-954-1126", "8-1039-1537", "8-925-1168", "8-940-1505", "8-938-925", "3-743-2493","E-8-169755",
             "8-971-1811", "8-940-408", "8-937-1640", "8-939-1578", "8-942-1406", "6-722-724", "8-941-1215", "8-962-1218", "8-941-924","8-980-2414", "3-742-2055",
             "8-942-1192", "8-904-1189", "8-959-857", "8-964-1554", "8-1066-1018", "8-1055-701", "8-970-242", "3-752-1461","3-745-950"};
     String[] nombres = {"Edwin Arrocha", "Michelle Brenes", "Eddy Bustamante", "Alejandra Caballero", "Gabriel Chiari", "Kevin Coronado", "Luis Delgado",
@@ -35,21 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
         etcedula = findViewById(R.id.etcedula);
         btn1 = findViewById(R.id.btn1);
-        btn2 = findViewById(R.id.btn2);
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Votos.class);
-                startActivity(intent);
-            }
-        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean valida = false;
                 int estudiante = 40;
-                for (int i = 0; i < 39; i++) {
+                for(int i = 0; i < 39; i++) {
                     /** valida que la cedula este en el vector */
                     if (etcedula.getText().toString().equals(cedulas[i])) {
                         valida = true;
@@ -57,17 +52,25 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     }
                 }
-                if (valida) {
-                    /** Aqui verifica si ya entro */
+                if (valida){
+                    /** Cuando entra, este le da la biencenida y menciona su nombre */
                     startActivity(new Intent(MainActivity.this, Eleccion.class));
-
                     Toast.makeText(MainActivity.this, ("Bienvenido " + nombres[estudiante]), Toast.LENGTH_LONG).show();
-                } else {
+
+                }else{
                     Toast.makeText(MainActivity.this, "Usted ya ha realizado su voto", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        /** se creo este boton para que los que ya votaron, puedan ver los resultados */
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Votos.class);
+                startActivity(intent);
             }
         });
 
     }
 }
-
