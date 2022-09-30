3f2591dc49656445ddb7b7db78a2685ef64bac18
Juliok04 <julio.navas@utp.ac.pa>
2022-09-29 21:19:16 -0500

    Arreglo de la eleccion y la validacion de entrada que no funcionaba bien

--- app/src/main/java/com/example/proyecto1zsjn/Eleccion.java
+++ app/src/main/java/com/example/proyecto1zsjn/Eleccion.java
@@ -14,6 +14,8 @@ import android.widget.RadioButton;
 public class Eleccion extends AppCompatActivity {
     RadioGroup  radioGroup;
     Button botonVoto;
+    RadioButton rb1, rb2, rb3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,40 +24,48 @@ public class Eleccion extends AppCompatActivity {
 
         radioGroup = findViewById(R.id.rg);
         botonVoto = findViewById(R.id.btn1);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        rb3 = findViewById(R.id.rb3);
+
+       /* radioGroup.clearCheck();
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup grupo, int id){
+                RadioButton rb = (RadioButton)grupo.findViewById(id);
+
+            }
+        });*/
 
 
         botonVoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                int checkedId = radioGroup.getCheckedRadioButtonId();
-                Intent intent =new Intent(Eleccion.this, Votos.class);
-                if (checkedId == -1) {
-                    Toast.makeText(Eleccion.this, "No hay seleccion.", Toast.LENGTH_SHORT).show();
-                }else{
-                    findRadioButton(checkedId);
+                if (rb1.isChecked() == true) {
+                    Usuario.cand1 =  Usuario.cand1 + 1;
+                    Intent intent = new Intent(getApplicationContext(), Votos.class);
+                    intent.putExtra("cand1", Usuario.cand1);
                     startActivity(intent);
                 }
+
+                if (rb2.isChecked() == true) {
+                    Usuario.cand2 =  Usuario.cand2 + 1;
+                    Intent intent = new Intent(getApplicationContext(), Votos.class);
+                    intent.putExtra("cand1", Usuario.cand2);
+                    startActivity(intent);
+                }
+                if (rb3.isChecked() == true) {
+                    Usuario.cand3 =  Usuario.cand3 + 1;
+                    Intent intent = new Intent(getApplicationContext(), Votos.class);
+                    intent.putExtra("cand1", Usuario.cand3);
+                    startActivity(intent);
+                }
+
 
             }
         });
 
 
     }
-    private void findRadioButton(int checkedId) {
-        switch (checkedId) {
-            case R.id.rb1:
-                Usuario.cand1++;
-                break;
-            case R.id.rb2:
-                Usuario.cand2++;
-                break;
-            case R.id.rb3:
-                Usuario.cand3++;
-                break;
-
-        }
-    }
 }
-
 
 
--- app/src/main/java/com/example/proyecto1zsjn/MainActivity.java
+++ app/src/main/java/com/example/proyecto1zsjn/MainActivity.java
@@ -18,17 +18,19 @@ public class MainActivity extends AppCompatActivity {
      * el bt2 es para ver cuanto porcentaje de votos tiene cada candidato, se crearon 2 Vectores que guardan los nombres y apellidos de los estudiantes
      * estan en orden para identificar de quien es cada cedula*/
 
-    EditText etcedula;
-    Button btn1, btn2;
-    String[] cedulas = {"8-944-327", "3-740-1394", "20-53-4282", "8-943-1867", "8-937-503", "8-952-2444", "8-943-12", "8-986-549", "8-957-1827",
-            "8-940-1311", "8-863-1620", "8-964-691", "8-954-1126", "8-1039-1537", "8-925-1168", "8-940-1505", "8-938-925", "3-743-2493","E-8-169755",
-            "8-971-1811", "8-940-408", "8-937-1640", "8-939-1578", "8-942-1406", "6-722-724", "8-941-1215", "8-962-1218", "8-941-924","8-980-2414", "3-742-2055",
-            "8-942-1192", "8-904-1189", "8-959-857", "8-964-1554", "8-1066-1018", "8-1055-701", "8-970-242", "3-752-1461","3-745-950"};
-    String[] nombres = {"Edwin Arrocha", "Michelle Brenes", "Eddy Bustamante", "Alejandra Caballero", "Gabriel Chiari", "Kevin Coronado", "Luis Delgado",
+    EditText etcedula; /** captura la cedula que ingresan*/
+    Button btn1, btn2; /** btn 1 es para ingresar y btn2 es para verificar los votos*/
+
+    public static String[] cedulas = {"8-944-327", "3-740-1394", "20-53-4282", "8-943-1867", "8-937-503", "8-952-2444", "8-943-12", "8-986-549", "8-957-1827",
+                    "8-940-1311", "8-863-1620", "8-964-691", "8-954-1126", "8-1039-1537", "8-925-1168", "8-940-1505", "8-938-925", "3-743-2493","E-8-169755",
+                    "8-971-1811", "8-940-408", "8-937-1640", "8-939-1578", "8-942-1406", "6-722-724", "8-941-1215", "8-962-1218", "8-941-924","8-980-2414", "3-742-2055",
+                    "8-942-1192", "8-904-1189", "8-959-857", "8-964-1554", "8-1066-1018", "8-1055-701", "8-970-242", "3-752-1461","3-745-950"};
+    public static String[] nombres = {"Edwin Arrocha", "Michelle Brenes", "Eddy Bustamante", "Alejandra Caballero", "Gabriel Chiari", "Kevin Coronado", "Luis Delgado",
             "Marc Dudley", "Diego Fernandez", "María Flores", "Donaldo Frazer", "Miguel Gascón", "Andrew Gonzalez", "Valerie Hernández", "Tommy Ho",
             "Jean Jiménez", "Felipe Loo", "Lisseth Mcdonald", "Jesus Monagas", "Diana Moreno", "Gerardo Moreno", "Juan Navarro", "Julio Navas", "Anel Pinzón",
             "Janiel Pinzón", "Antonio Qiu", "Carlos Ricord", "David Rios", "Sergio Rodriguez", "Zulixa Salas", "Luis Samaniego", "Ary Sánchez","Dionisio Santos",
             "Bryan Thomas", "Carlos Tuñón", "Genesis Vitale", "Fernando Zeng", "Carlos Zhong","Eimy Guerra"};
+
 
 
     @Override
@@ -42,23 +44,25 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean valida = false;
-                int estudiante = 40;
+                boolean valida = false; /** Variable para validar cuando entra un usuario*/
+                int estudiante = 40; /** Numero de estudiantes*/
+
+                /** Verifica si la cedula ingresada se encuentra en el vector de cedulas */
                 for(int i = 0; i < 39; i++) {
-                    /** valida que la cedula este en el vector */
-                    if (etcedula.getText().toString().equals(cedulas[i])) {
+                    if (etcedula.getText().toString().equals(MainActivity.cedulas[i])) {
+                        MainActivity.cedulas[i] = null; /** Esto hace que una vez que entre la cedula del estudiante, la convierta en Null para que ya no pueda acceder*/
                         valida = true;
                         estudiante = i;
                         break;
                     }
                 }
                 if (valida){
-                    /** Cuando entra, este le da la biencenida y menciona su nombre */
+                    /** Cuando entra, este le da la bienvenida y menciona su nombre */
                     startActivity(new Intent(MainActivity.this, Eleccion.class));
-                    Toast.makeText(MainActivity.this, ("Bienvenido " + nombres[estudiante]), Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, ("Bienvenido " + MainActivity.nombres[estudiante]), Toast.LENGTH_LONG).show();
 
                 }else{
-                    Toast.makeText(MainActivity.this, "Usted ya ha realizado su voto", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "No tiene acceso", Toast.LENGTH_SHORT).show();
                 }
             }
         });
